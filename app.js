8e6437e0bfda7fd3da266a99af8875fb3e0e4605
LedaFormigoni <leda.m@aluno.senai.br>
2025-10-15 11:27:01 -0300

    aula 15/10

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 import express from "express";
 import cors from "cors";
-import { listaProdutos } from "./produtos.js";
+import sql from './database.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Rota para trazer todos os produtos
-app.get("/produtos", (requisicao, resposta) => {
-  return resposta.status(200).json(listaProdutos);
+app.get("/produtos",async (requisicao, resposta) => {
+  const produtos =  await sql`SELECT * FROM produtos`;
+  return resposta.status(200).json(produtos);
 });
 
-app.get("/produtos/:id", (req, res) => {
+app.get("/produtos/:id", async (req, res) => {
   const { id } = req.params;
-  const produto = listaProdutos.find((p) => p.id == id);
-  return res.status(200).json(produto);
+  const produto =  await sql`SELECT * FROM produtos WHERE id = ${id}`;
+  return res.status(200).json(produto[0]);
 });
 
 app.listen(3000, () => {
